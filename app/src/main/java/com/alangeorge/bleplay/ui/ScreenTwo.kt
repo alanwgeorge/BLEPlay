9ce5e5809769086aff3d4c660b93e64c1b5c3714
Alan George <alan.george@abbott.com>
2021-12-11 08:28:21 -0800

    scaffold play WIP

--- app/src/main/java/com/alangeorge/bleplay/ui/ScreenTwo.kt
+++ app/src/main/java/com/alangeorge/bleplay/ui/ScreenTwo.kt
@@ -1,9 +1,7 @@
 package com.alangeorge.bleplay.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
@@ -11,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -38,3 +37,54 @@ fun ScreenTwo() {
 fun ScreenTwoPreview() {
     ScreenTwo()
 }
+
+@Composable
+fun CustomLayoutPlay() {
+    CustomLayout(
+        top = {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Text(text = "Top Bar")
+            }
+        },
+        bottom = {
+            Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                Text(text = "Bottom Bar")
+            }
+        }
+    ) {
+        Box(modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()) {
+            Text(text = "Context")
+        }
+    }
+}
+
+@Composable
+fun CustomLayout(
+    modifier: Modifier = Modifier,
+    top: @Composable () -> Unit,
+    bottom: @Composable () -> Unit,
+    content: @Composable () -> Unit,
+) {
+    SubcomposeLayout { constraints ->
+        val contentPlaceables = subcompose(slotId = SlotsEnum.Content, content = content).map {
+            it.measure(constraints = constraints)
+        }
+
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            contentPlaceables.forEach {
+                it.placeRelative(0, 0)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CustomLayoutPlayPreview() {
+    CustomLayoutPlay()
+}
+
+enum class SlotsEnum {Top, Bottom, Content}
