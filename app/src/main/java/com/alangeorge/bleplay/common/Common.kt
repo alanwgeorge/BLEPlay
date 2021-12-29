0849bad1d23a595a960e83d65ea8902754a46cd6
alanwgeorge <ageorge@alan-george.com>
2021-12-29 13:38:57 -0800

    getActivity()

--- app/src/main/java/com/alangeorge/bleplay/common/Common.kt
+++ app/src/main/java/com/alangeorge/bleplay/common/Common.kt
@@ -1,6 +1,21 @@
 package com.alangeorge.bleplay.common
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+
 fun ByteArray.toHexString(): String =
     joinToString(separator = " ", prefix = "0x") {
         String.format("%02X", it)
     }
+
+fun Context.getActivity(): Activity? {
+    var currentContext = this
+    while (currentContext is ContextWrapper) {
+        if (currentContext is Activity) {
+            return currentContext
+        }
+        currentContext = currentContext.baseContext
+    }
+    return null
+}
