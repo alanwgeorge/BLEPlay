351192f5ce5a2014908a8be978e04f68bc401fb5
alanwgeorge <ageorge@alan-george.com>
2022-02-01 08:07:58 -0800

    fix imports

--- app/src/main/java/com/alangeorge/bleplay/MainActivity.kt
+++ app/src/main/java/com/alangeorge/bleplay/MainActivity.kt
@@ -3,35 +3,10 @@ package com.alangeorge.bleplay
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.Text
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavType
-import androidx.navigation.navArgument
-import androidx.navigation.navigation
-import com.alangeorge.bleplay.ui.*
-import com.alangeorge.bleplay.ui.theme.BLEPlayTheme
-import com.alangeorge.bleplay.viewmodel.BleDeviceViewModel
-import com.alangeorge.bleplay.viewmodel.BleViewModel
-import com.alangeorge.bleplay.viewmodel.asScanData
-import com.google.accompanist.insets.ProvideWindowInsets
-import com.google.accompanist.insets.systemBarsPadding
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @ExperimentalComposeUiApi
 @ExperimentalAnimationApi
